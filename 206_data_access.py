51438ec7d023689c92de413c1b117c99c082cb20
migmartinez <migmart@umich.edu>
2017-04-17 23:06:42 -0400

    done but unpolished for data access

--- 206_data_access.py
+++ 206_data_access.py
@@ -68,10 +68,7 @@ def get_twitter_cache(handle):
 		f.write(json.dumps(TWITTER_CACHE_DICTION))
 		f.close()
 
-	twenty_tweets = []
-	for tweet in twitter_results:
-		twenty_tweets.append(tweet)
-	return twenty_tweets[:20]
+	return twitter_results
 
 ## OMBD CACHING BELOW
 OMDB_CACHE_FNAME = "SI206_finalproject_OMBDcache.json"
@@ -91,17 +88,130 @@ def get_movie_cache(name):
 		omdb_results = OMDB_CACHE_DICTION[unique_identifier]
 	else:
 		print('getting data from internet for movie', name)
-		omdb_results = omdb.search_movie(name)
+		omdb_results = omdb.title(name)
 		OMDB_CACHE_DICTION[unique_identifier] = omdb_results
 		g = open(OMDB_CACHE_FNAME, 'w')
 		g.write(json.dumps(OMDB_CACHE_DICTION))
 		g.close()
+	return omdb_results
 
+## STAR CACHING BELOW TO ANALYZE JSON OBJECT
+STAR_CACHE_FNAME = "SI206_finalproject_starcache.json"
+try:
+	star_cache_file = open(STAR_CACHE_FNAME,'r')
+	star_cache_contents = star_cache_file.read()
+	star_cache_file.close()
+	STAR_CACHE_DICTION = json.loads(star_cache_contents)
+except:
+	STAR_CACHE_DICTION = {}
 
-#Invoking 
+##Function to search star actor of movie
+def star_actor_tweets(searchterm):
+	unique_identifier = "star_{}".format(searchterm)
+	if unique_identifier in STAR_CACHE_DICTION:
+		print('using cached data for star', searchterm)
+		star_results = STAR_CACHE_DICTION[unique_identifier]
+	else:
+		print('getting data from tweets from Twitter for star', searchterm)
+		api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+		star_results = api.search(q = searchterm, count = 5)
+		STAR_CACHE_DICTION[unique_identifier] = star_results
+		r = open(STAR_CACHE_FNAME, 'w')
+		r.write(json.dumps(STAR_CACHE_DICTION))
+		r.close()
+	return star_results
+#Invoking to test cache files
 
 wizard_oz = get_movie_cache("The Wizard of Oz")
 umich = get_twitter_cache("umich")
+actor = star_actor_tweets("Judy Garland")
+
+#print(actor)
+#print(wizard_oz)
+
+#Class Setup
+class Movie():
+	def __init__(self, movie_dict={}):
+		if 'imdb_id' in movie_dict:
+			self.imdb_id = movie_dict['imdb_id']
+		else:
+			self.imdb_id = ""
+
+		if 'title' in movie_dict:
+			self.title = movie_dict['title']
+		else:
+			self.title = ""
+
+		if 'director' in movie_dict:
+			self.director = movie_dict['director']
+		else:
+			self.director = ""
+
+		if 'imdb_rating' in movie_dict:
+			self.imdb_rating = movie_dict['imdb_rating']
+		else:
+			self.imdb_rating = ""
+
+		if 'actors' in movie_dict:
+			self.actors = movie_dict['actors'].split(',')
+		else:
+			self.actors = []
+
+		if 'language' in movie_dict:
+			self.language = len(movie_dict['language'].split())
+		else:
+			self.language = 0
+
+class TwitterUser():
+	def __init__(self, tweet_dict):
+		if 'screen_name' in tweet_dict[0]['user']:
+			self.screen_name = tweet_dict[0]['user']['screen_name']
+		else:
+			self.screen_name = ""
+
+		if 'id' in tweet_dict[0]['user']:
+			self.id = tweet_dict[0]['user']['id']
+		else:
+			self.id = 0
+
+		if 'favourites_count' in tweet_dict[0]['user']:
+			self.favourites_count = tweet_dict[0]['user']['favourites_count']
+		else:
+			self.favourites_count = 0
+	
+
+class Tweet():
+	def __init__(self, tweet_dict):
+		if 'text'
+
+
+#Class invocation
+wizard_class = Movie(wizard_oz)
+twitteruser_class = TwitterUser(umich)
+#print(wizard_class.actors)
+#print(twitteruser_class.id)
+
+#Database file setup
+conn = sqlite3.connect('206_finalproject.db')
+cur = conn.cursor()
+cur.execute('DROP TABLE IF EXISTS Tweets')
+cur.execute('DROP TABLE IF EXISTS Users')
+cur.execute('DROP TABLE IF EXISTS Movies')
+cur.execute('CREATE TABLE Tweets (tweet_id TEXT PRIMARY KEY, text TEXT, screen_name TEXT, movie_title TEXT, num_favs INTEGER, retweets INTEGER, FOREIGN KEY (screen_name) REFERENCES Users(screen_name), FOREIGN KEY (movie_title) REFERENCES Movies(movie_title))')
+cur.execute('CREATE TABLE Users (user_id TEXT PRIMARY KEY, screen_name TEXT, num_favs INTEGER)')
+cur.execute('CREATE TABLE Movies (movie_id TEXT PRIMARY KEY, movie_title TEXT, director TEXT, num_languages INTEGER, imdb_rating INTEGER, top_actor TEXT)')
+
+#Adding data to User table
+cur.execute("INSERT INTO Users (user_id, screen_name, num_favs) VALUES (?, ?, ?)", (twitteruser_class.id, twitteruser_class.screen_name, twitteruser_class.favourites_count))
+conn.commit()
+
+#Adding data to Movie table
+cur.execute("INSERT INTO Movies (movie_id, movie_title, director, num_languages, imdb_rating, top_actor) VALUES (?, ?, ?, ?, ?, ?)", (wizard_class.imdb_id, wizard_class.title, wizard_class.director, wizard_class.language, wizard_class.imdb_rating, wizard_class.actors[0]))
+conn.commit()
+
+#Adding data to Tweets table
+
+
 # Put your tests here, with any edits you now need from when you turned them in with your project plan.
 class Task1(unittest.TestCase):
 	def test_twitter_cache(self):
@@ -116,31 +226,31 @@ class Task1(unittest.TestCase):
 		self.assertEqual(type(s), type(""), "Caching is not working correctly for OMBD")
 	def test_movie_str(self):
 		m = Movie("The Wizard of Oz")
-		self.asssertEqual(type(m.__str__()), type(""), "testing type of __str__ method, should be str")
+		self.assertEqual(type(m.__str__()), type(""), "testing type of __str__ method, should be str")
 	def test_num_languages(self):
-		m = Movie("The Wizard of Oz")
-		self.assertEqual(m.find_languages(), 1, "testing that wizard of oz has 1 language")
+		wizard_class = Movie(wizard_oz)
+		self.assertEqual(wizard_class.language, 1, "testing that wizard of oz has 1 language")
 	def test_users_table(self):
-		conn = sqlite3.connect('finalproject.db')
+		conn = sqlite3.connect('206_finalproject.db')
 		cur = conn.cursor()
 		cur.execute('SELECT * FROM Users');
 		result = cur.fetchall()
 		self.assertTrue(len(result)>=1, "Testing there is at least 1 user in the User table")
 	def test_tweets_table(self):
-		conn = sqlite3.connect('finalproject.db')
+		conn = sqlite3.connect('206_finalproject.db')
 		cur = conn.cursor()
 		cur.execute('SELECT * FROM Tweets');
 		result = cur.fetchall()
 		self.assertTrue(len(result[0])==6, "Testing that there are 6 columnns in the Tweets table")
 	def test_movies_table(self):
-		conn = sqlite3.connect('finalproject.db')
+		conn = sqlite3.connect('206_finalproject.db')
 		cur = conn.cursor()
 		cur.execute('SELECT * FROM Movies');
 		result = cur.fetchall()
 		self.assertTrue(len(result[0])==6, "Testing that there are 6 columns in the Movies table")
 	def test_top_billed(self):
-		m = Movie("The Wizard of Oz")
-		self.assertTrue("Judy Garland" in m.actors, "Testing that Judy Garland is correctly in the actors list for the Wizard of Oz movie")
+		wizard_class = Movie(wizard_oz)
+		self.assertTrue("Judy Garland" in wizard_class.actors, "Testing that Judy Garland is correctly in the actors list for the Wizard of Oz movie")
 
 # Remember to invoke your tests so they will run! (Recommend using the verbosity=2 argument.)
 if __name__ == "__main__":
